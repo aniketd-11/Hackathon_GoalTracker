5fb9dce5d3dba3f65572dd87923b2f36da864d87
Aniket.Desai <aniket.desai@harbingergroup.com>
2024-10-02 16:42:43 +0530

    Get tracker action api added

--- backend/src/main/java/com/goaltracker/controller/TrackerController.java
+++ backend/src/main/java/com/goaltracker/controller/TrackerController.java
@@ -2,15 +2,16 @@ package com.goaltracker.controller;
 
 import com.goaltracker.dto.GoalTrackerRequestDTO;
 import com.goaltracker.model.GoalTrackerMaster;
+import com.goaltracker.model.TemplateAction;
+import com.goaltracker.model.TemplateTypes;
 import com.goaltracker.service.Interface.AccountService;
 import com.goaltracker.service.Interface.ProjectService;
 import com.goaltracker.service.Interface.TrackerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tracker")
@@ -31,6 +32,17 @@ public class TrackerController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Error occurred while creating Goal Tracker: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/get-trackerActions")
+    public ResponseEntity<?> getTrackerActions(@RequestParam TemplateTypes templateType){
+        try {
+            List<TemplateAction> templateActions = trackerService.getTemplateActions(templateType);
+            return ResponseEntity.ok(templateActions);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error occurred getting Goal Tracker Actions: " + e.getMessage());
         }
     }
 }
--- backend/src/main/java/com/goaltracker/service/Interface/TrackerService.java
+++ backend/src/main/java/com/goaltracker/service/Interface/TrackerService.java
@@ -2,8 +2,13 @@ package com.goaltracker.service.Interface;
 
 import com.goaltracker.dto.GoalTrackerRequestDTO;
 import com.goaltracker.model.GoalTrackerMaster;
+import com.goaltracker.model.TemplateAction;
+import com.goaltracker.model.TemplateTypes;
+
+import java.util.List;
 
 public interface TrackerService {
 
     GoalTrackerMaster addGoalTracker(GoalTrackerRequestDTO dto);
+    List<TemplateAction> getTemplateActions(TemplateTypes templateType);
 }
