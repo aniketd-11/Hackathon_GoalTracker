85db463f9787ed310522f954c7450ba83e8bbfa9
Aniket Desai <aniketd936@gmail.com>
2024-10-20 14:01:09 +0530

    Backend API changes

--- backend/src/main/java/com/goaltracker/service/Interface/TrackerService.java
+++ backend/src/main/java/com/goaltracker/service/Interface/TrackerService.java
@@ -1,9 +1,6 @@
 package com.goaltracker.service.Interface;
 
-import com.goaltracker.dto.ActionValueDTO;
-import com.goaltracker.dto.GoalTrackerDTO;
-import com.goaltracker.dto.GoalTrackerRequestDTO;
-import com.goaltracker.dto.ProjectWithGoalTrackerDTO;
+import com.goaltracker.dto.*;
 import com.goaltracker.model.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,5 +17,6 @@ public interface TrackerService {
     void updateGoalTrackerStatus(int trackerId, Status status);
     void updateGoalTrackerRating(int trackerId, Rating rating);
     List<ProjectWithGoalTrackerDTO> getAllProjectsWithGoalTrackers(TemplateTypes templateType);
-    GoalTrackerDTO addNoteToTracker(int trackerId, String note);
+    GoalTrackerDTO addNoteToTracker(int trackerId, NoteRequestDTO dto);
+    ActionValueDTO addActionPlanByTracker(int trackerId, ActionPlanRequestDTO dto);
 }
