cdfcdae5cd785e49d77dbefa92e3435fdf155657
Shreyash Arali <shreyash.arali@harbingergroup.com>
2025-01-06 20:04:49 +0530

    nginx.conf

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
+
+async function headers() {
+  return [
+    {
+      source: "/api/(.*)", // Matches all API routes
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "no-store", // Forces all API routes to be dynamic
+        },
+      ],
+    },
+  ];
+}
+
 const nextConfig = {
   images: {
-    domains: ["localhost"], // Add your domain here
+    domains: ["localhost"],
   },
+  headers,
 };
 
 export default nextConfig;
